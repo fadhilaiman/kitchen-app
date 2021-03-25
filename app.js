9bc94fcdd86fd6a239ebbcbbf7d4f3bdf0d35f4e
Mohammad Fadhil Aiman <mfadhilaiman@gmail.com>
2021-03-26 03:37:52 +0700

    test2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = require('./routes/index');
 const session = require('express-session');
 
